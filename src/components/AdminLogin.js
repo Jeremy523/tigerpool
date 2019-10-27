3a83ac593399e6cffe24a07678b0fe9e95d35b44
Christopher Savan <cxs7700@rit.edu>
2019-10-27 17:50:48 -0400

    Using alerts instead of logging when validating

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -12,8 +12,6 @@ class AdminLogin extends React.Component {
     this.testEmail = "admin@tigerpool.com"
     this.testPassword = "admin123"
     this.emailValidator = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}'
-
-
   }
 
 
@@ -21,12 +19,15 @@ class AdminLogin extends React.Component {
  handleSubmit(e) {
     e.preventDefault();
     if (RegExp(this.emailValidator).test(this.state.email)){
-        // log when a good email that is not the test email is entered
-        console.log("Submit Clicked but Invalid")
-        if (this.state.email === this.testEmail && this.state.password === this.testPassword){
-            // log when a good email that is the test email is entered
-            console.log("VALID EMAIL")
-        }
+      // log when a good email that is not the test email is entered
+      // console.log("Submit Clicked but Invalid")
+      if (this.state.email === this.testEmail && this.state.password === this.testPassword){
+        // log when a good email that is the test email is entered
+        alert("Logged in as admin")
+      }
+      else {
+        alert("Submit clicked, but email is invalid")
+      }
     }
   }
 
@@ -45,17 +46,17 @@ class AdminLogin extends React.Component {
         <div className="jumbotron">
           <h1 className="display-4 text-center">Admin Login</h1>
         </div>
-        <form class="w-50 m-3 mx-auto" onSubmit={this.handleSubmit.bind(this)}>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input name="email" value={this.state.email} onChange={this.handleChange.bind(this)} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                <small id="emailHelp" class="form-text text-muted"></small>
+        <form className="w-50 m-3 mx-auto" onSubmit={this.handleSubmit.bind(this)}>
+            <div className="form-group">
+                <label htmlFor="inputEmail">Email address</label>
+                <input name="email" value={this.state.email} onChange={this.handleChange.bind(this)} type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                <small id="emailHelp" className="form-text text-muted"></small>
             </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input name="password" value={this.state.password} onChange={this.handleChange.bind(this)} type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+            <div className="form-group">
+                <label htmlFor="inputPassword">Password</label>
+                <input name="password" value={this.state.password} onChange={this.handleChange.bind(this)} type="password" className="form-control" id="inputPassword" placeholder="Password"></input>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-dark">Submit</button>
         </form>
       </div>
     )
