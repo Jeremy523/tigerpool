a220c171b5e32e79567473997703bd2e475d3061
jmrossi98 <jmrossi98@gmail.com>
2019-10-27 21:49:50 -0400

    Added Admin Dashboard that appears upon login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 import Navbar from './components/Navbar';
+import AdminDash from './components/AdminDash';
 
 function App() {
   
   return (
     <div className="App">
       <Navbar />
+	  <AdminDash />
     </div>
   );
 }
