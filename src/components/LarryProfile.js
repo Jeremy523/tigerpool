8c741a78026d981a827daa3c07b6d214b0d0ca20
cxs7700 <cxs7700@rit.edu>
2019-11-08 16:14:28 -0500

    larry bold header

--- src/components/LarryProfile.js
+++ src/components/LarryProfile.js
@@ -5,8 +5,8 @@ const Larry = () => {
   return (
     <div className="card" style={{'width': '18rem'}}>
       <img className="card-img-top" src={Boyfriend} />
-      <div className="card-body">
-        <h5 className="card-title">Smalltown Larry</h5>
+      <div className="card-body text-center">
+        <h5 className="card-title"><strong>Smalltown Larry</strong></h5>
         <h6 className="card-subtitle mb-4 text-muted">Registered Driver</h6>
         <p className="card-text mb-0"><strong>Age: </strong>22</p>
         <p className="card-text mb-0"><strong>Vehicle: </strong>Honda Civic</p>
