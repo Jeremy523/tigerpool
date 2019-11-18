127eaea444428150e0b82b6f222949930c5b691c
cxs7700 <cxs7700@rit.edu>
2019-11-18 15:18:26 -0500

    rate driver stuff

--- src/pages/RateDriver.js
+++ src/pages/RateDriver.js
@@ -3,10 +3,21 @@ import Larry from '../components/Larry'
 
 const RateDriver = () => {
   return (
-    <div className="container">
-      <div className="rate-driver-wrapper">
-        <Larry />
-        
+    <div className="rate-driver-component">
+      <div className="jumbotron">
+        <h1 className="display-4 text-center">Rate Your Driver</h1>
+      </div>
+      <div className="container">
+        <div className="rate-driver-wrapper">
+          <Larry />
+          <div className="rate-driver-content">
+            <span><i className="material-icons star">star_border</i></span>
+            <span><i className="material-icons star">star_border</i></span>
+            <span><i className="material-icons star">star_border</i></span>
+            <span><i className="material-icons star">star_border</i></span>
+            <span><i className="material-icons">star_border</i></span>
+          </div>
+        </div>
       </div>
     </div>
   )
