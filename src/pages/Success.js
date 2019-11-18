73b2efc7cf9374128a30b0d1b7afe4e6c0fd91fa
cxs7700 <cxs7700@rit.edu>
2019-11-18 16:23:33 -0500

    Back to home button

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class Success extends React.Component {
   
@@ -8,6 +9,9 @@ class Success extends React.Component {
         <div className="success-component">
           <h2>Your driver has received your feedback.</h2>
           <h2>Thank you for choosing Tigerpool!</h2>
+          <Link to="/" className="btn btn-dark btn-md px-4 mt-4">
+            Back to Home
+          </Link>
         </div>
       </div>
     )
