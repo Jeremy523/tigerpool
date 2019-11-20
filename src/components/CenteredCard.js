fc304566235ee40bc95e048890aeb5780247493a
Jeremy De La Cruz <jeremydelacruz219@gmail.com>
2019-11-20 01:52:57 -0500

    CSS classname changes to avoid conflict

--- src/components/CenteredCard.js
+++ src/components/CenteredCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Card, CardTitle, Row, Col } from 'reactstrap';
 
 const CenteredCard = ({ children, title: title }) => (
-    <Row className="card-row">
+    <Row className="form-card-row">
         <Col sm="3"></Col>
         <Col sm="6">
             <Card body>
-                <CardTitle className="card-title">{title}</CardTitle>
+                <CardTitle className="form-card-title">{title}</CardTitle>
                 {children}
             </Card>
         </Col>
