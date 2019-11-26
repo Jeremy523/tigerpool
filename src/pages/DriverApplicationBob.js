94096fc4f9eb1d09b4bdd5e0e98ee2ec874c6c24
cxs7700 <cxs7700@rit.edu>
2019-11-26 15:35:05 -0500

    Text change

--- src/pages/DriverApplicationBob.js
+++ src/pages/DriverApplicationBob.js
@@ -43,7 +43,7 @@ const DriverApplicationBob = () => {
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="modalLongTitle">Action Confirmation for Driver Application</h5>
+                <h5 class="modal-title" id="modalLongTitle">Action Confirmation</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
--- src/pages/DriverApplicationLarry.js
+++ src/pages/DriverApplicationLarry.js
@@ -39,7 +39,7 @@ const DriverApplicationLarry = () => {
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="modalLongTitle">Action Confirmation for Driver Application</h5>
+                <h5 class="modal-title" id="modalLongTitle">Action Confirmation</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
