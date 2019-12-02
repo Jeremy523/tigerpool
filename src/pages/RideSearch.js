86c8c6c540038c6a7a721bed3e0fb99f8911e7df
cxs7700 <cxs7700@rit.edu>
2019-12-02 14:20:19 -0500

    one map onl

--- src/pages/RideSearch.js
+++ src/pages/RideSearch.js
@@ -49,7 +49,7 @@ class RideSearch extends React.Component {
                 <div className="search text-center">
                     <div className="search-items">
                         <div>
-                            <div class="input-group mb-3 input-group-lg">
+                            <div class="input-group mb-3 input-group-lg input-item">
                                 <div class="input-group-append">
                                     <span class="input-group-text" >
                                         <i class="material-icons">calendar_today</i>
@@ -59,7 +59,7 @@ class RideSearch extends React.Component {
                             </div>
                         </div>
                         <div>
-                            <div class="input-group mb-3 input-group-lg">
+                            <div class="input-group mb-3 input-group-lg input-item">
                                 <div class="input-group-append">
                                     <span class="input-group-text">
                                         <i class="material-icons">my_location</i>
@@ -68,12 +68,11 @@ class RideSearch extends React.Component {
                                 <input type="location" class="form-control" placeholder="Current Location" required onChange={e => this.handleStartChange(e.currentTarget.value)} value={this.state.startLocation} aria-describedby="basic-addon1" />
                             </div>
                         </div>
-                        <Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39237.99028693614!2d-77.67252117661!3d43.07809800287481!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d14c603a147e55%3A0xbe7eb31ed5e22c03!2sRochester%20Institute%20of%20Technology!5e0!3m2!1sen!2sus!4v1573345631275!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></Iframe>
                     </div>
                     <div className="search-items">
                         <div className="search">
                             <div>
-                                <div class="input-group mb-3 input-group-lg">
+                                <div class="input-group mb-3 input-group-lg input-item">
                                     <div class="input-group-append">
                                         <span class="input-group-text" id="basic-addon1">
                                             <i class="material-icons">access_time</i>
@@ -83,7 +82,7 @@ class RideSearch extends React.Component {
                                 </div>
 
                                 <div>
-                                    <div class="input-group mb-3 input-group-lg">
+                                    <div class="input-group mb-3 input-group-lg input-item">
                                         <div class="input-group-append">
                                             <span class="input-group-text" >
                                                 <i class="material-icons">map</i>
@@ -92,12 +91,14 @@ class RideSearch extends React.Component {
                                         <input type="location" class="form-control" placeholder="Destination" required value={this.state.endLocation} onChange={e => this.handleEndChange(e.currentTarget.value)} aria-describedby="basic-addon1" />
                                     </div>
                                 </div>
-
-                                <Iframe src="https://www.google.com/maps/embed?" width="600" height="450" frameborder="0" style="border:0;" ></Iframe>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div className="container">
+                    <Iframe className="mx-auto" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39237.99028693614!2d-77.67252117661!3d43.07809800287481!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d14c603a147e55%3A0xbe7eb31ed5e22c03!2sRochester%20Institute%20of%20Technology!5e0!3m2!1sen!2sus!4v1573345631275!5m2!1sen!2sus" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen=""></Iframe>
+                </div>
+
                 <div className="text-center">
                     <Link to="/listings" className="mt-4 mb-4 btn btn-dark btn-lg login-element px-4">
                         Find Trip
