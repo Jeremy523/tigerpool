cf338b173bca123435d3aefc108089d07d0883ee
Jeremy De La Cruz <jeremydelacruz219@gmail.com>
2019-12-04 09:41:09 -0500

    Updates

---
+++ precache-manifest.a429b4bcdf9810aa3b019174c20dedd0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3709dfc52ffc6b0cf324eaf97d9e50",
+    "url": "/tigerpool/index.html"
+  },
+  {
+    "revision": "de5f242565385acfa19a",
+    "url": "/tigerpool/static/css/main.9ec8d9c6.chunk.css"
+  },
+  {
+    "revision": "10f5c34a515d2edc0db8",
+    "url": "/tigerpool/static/js/2.1d4c7752.chunk.js"
+  },
+  {
+    "revision": "de5f242565385acfa19a",
+    "url": "/tigerpool/static/js/main.f0871356.chunk.js"
+  },
+  {
+    "revision": "eddc57631bc132abd92a",
+    "url": "/tigerpool/static/js/runtime-main.f1e80453.js"
+  },
+  {
+    "revision": "20d5246dd111da5e8dd1a46cd89983fa",
+    "url": "/tigerpool/static/media/Bob.20d5246d.png"
+  },
+  {
+    "revision": "60e8a1da2c4897330e65cf77be02a117",
+    "url": "/tigerpool/static/media/Ford1.60e8a1da.jpg"
+  },
+  {
+    "revision": "f3ad590a7a858363fb29cb2c2d99b35b",
+    "url": "/tigerpool/static/media/Ford2.f3ad590a.jpg"
+  },
+  {
+    "revision": "e4fe5cab757bcb9cd1c4210673571cec",
+    "url": "/tigerpool/static/media/Honda1.e4fe5cab.png"
+  },
+  {
+    "revision": "08cf5088baa177ecc822a9e022d4ba89",
+    "url": "/tigerpool/static/media/Honda2.08cf5088.png"
+  },
+  {
+    "revision": "f8c7d161ca46e127085f70f9c1cf62d2",
+    "url": "/tigerpool/static/media/Larry.f8c7d161.png"
+  }
+]);
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import Tigerpool from './Tigerpool';
-
-ReactDOM.render(<Tigerpool basename={process.env.PUBLIC_URL} />, document.getElementById('root'));
-
-serviceWorker.unregister();
